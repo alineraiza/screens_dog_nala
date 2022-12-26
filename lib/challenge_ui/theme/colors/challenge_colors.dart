e295c1915b2eb58bfab3a4d87cab3c5c6eb7f32f
Aline Raiza <alineraizagodoyrodrigues@gmail.com>
2022-12-26 10:03:43 -0300

    style: :art: color file name improvement and new class assets

--- lib/challenge_ui/theme/colors/challenge_colors.dart
+++ lib/challenge_ui/theme/colors/challenge_colors.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 
 class ChallengeColors {
-  static const primary = Color(0XFFFFFFFFF);
+  static const primary = Color(0XFFFFFFFF);
   static const onPrimary = Color(0XFF6F6F6F);
   static const surface = Color(0XFFF2F2F2);
   static const onSurface = Color(0XFF282725);
   static const secondary = Color(0XFFFD6050);
   static const tertiary = Color(0XFFCBCBCB);
 }
-
